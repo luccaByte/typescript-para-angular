0e85840b3f0c1cadd111d61b39115a7614939a51
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-02 16:17:53 +0000

    Aprendendo tipos de variáveis no TypeScript

---
+++ build/tiposVariáveis01.js
@@ -0,0 +1,17 @@
+"use strict";
+// TIPOS PRIMITIVOS: 
+// boolean
+let ligado = false;
+// number
+let idade = 23;
+let altura = 1.80;
+// string
+let nome = "Lucas";
+// TIPOS ESPECIAIS:
+// null / nundefined
+let nulo = null;
+let indefinido = undefined;
+// TIPOS ABRANGENTES:
+// any / void
+let retorno;
+let retornoView;
---
+++ src/tiposVariáveis01.ts
@@ -0,0 +1,23 @@
+// TIPOS PRIMITIVOS: 
+
+// boolean
+let ligado: boolean = false;
+
+// number
+let idade: number = 23;
+let altura: number = 1.80;
+
+// string
+let nome: string = "Lucas";
+
+// TIPOS ESPECIAIS:
+
+// null / nundefined
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+// TIPOS ABRANGENTES:
+
+// any / void
+let retorno: void 
+let retornoView: any 
