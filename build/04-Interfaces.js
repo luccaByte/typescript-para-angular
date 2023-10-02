565f6d6b6fdaa834408021430756c7bb2acd2336
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-02 17:35:36 +0000

    Type x Interface

--- build/04-Interfaces.js
+++ build/04-Interfaces.js
@@ -7,7 +7,22 @@ const bot1 = {
 };
 const bot2 = {
     id: 1,
-    name: "Robô"
+    name: "Robô",
+    sayHello: function () {
+        throw new Error("Função não implementada.");
+    },
 };
 console.log(bot1);
 console.log(bot2);
+// Quando usar Interfaces
+class Pessoa {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    sayHello() {
+        return (`hello i'm ${this.name}`);
+    }
+}
+const p = new Pessoa(1, "gutsman");
+console.log(p.sayHello());
--- src/04-Interfaces.ts
+++ src/04-Interfaces.ts
@@ -11,6 +11,7 @@ type robot = {
 interface robot2 {
     readonly id: number | string;
     name: string;
+    sayHello(): string;
 };
 
 const bot1: robot = {
@@ -20,8 +21,30 @@ const bot1: robot = {
 
 const bot2: robot2 = {
     id: 1,
-    name: "Robô"
+    name: "Robô", 
+    sayHello: function():string {
+        throw new Error("Função não implementada.");
+    },
 };
 
 console.log (bot1);
 console.log (bot2);
+
+
+// Quando usar Interfaces
+
+class Pessoa implements robot2 {
+    id: string | number;
+    name: string;
+
+    constructor(id: string | number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+    sayHello(): string {
+        return (`hello i'm ${this.name}`);
+    }
+}
+
+const p = new Pessoa (1, "gutsman")
+console.log(p.sayHello());
